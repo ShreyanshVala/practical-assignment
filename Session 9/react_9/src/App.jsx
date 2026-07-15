05e6db3d58ad65d952b5eb834077beec95376206
ShreyanshVala <125125984+ShreyanshVala@users.noreply.github.com>
2026-07-15 16:35:58 +0530

    session 9 add

---
+++ Session 9/react_9/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import {app} from "./firebase";
+
+function App() {
+  console.log(app);
+
+  return (
+    <div className="App">
+      <h1>Firebase Connected Successfully</h1>
+    </div>
+  );
+}
+
+export default App;
